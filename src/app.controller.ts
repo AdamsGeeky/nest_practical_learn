9953048020c0326741130cda1681dca806d87647
Adamu Muhammad Muhammad <muhdadamu0008@gmail.com>
2026-05-07 21:41:07 +0100

    feat: user Croud with croud Generator and syn with App.module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,17 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('/health')
+  getHealth(): string {
+    return 'Service is running!';
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('welcome')
+  getWelcomeMessage(): string {
+    return this.appService.getWelcomeMessage();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,7 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getWelcomeMessage(): string {
+    return 'Welcome to our application!';
+  }
 }
